0d642819f73eb59f5b4798146c08a04ab17526fc
Tanya-Amber-L <tanya.leenders@gmail.com>
2021-10-21 10:22:28 +0200

    style in progress skills, exp and portfolio sections

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -21,11 +21,23 @@ skillsBtns.forEach(function (button, i) {
   });
 });
 
-function resetClasses() {
+var resetClasses = function resetClasses() {
   skillsContents.forEach(function (content) {
     content.classList.remove("show-content");
   });
   skillsBtns.forEach(function (button) {
     button.classList.remove("content-clicked");
   });
-}
+}; // MOVE BLOB TO MOUSE CLICK POSITION IN THE SKILLS NAV CONTAINER
+// const skillsNavContainer = document.getElementById("skills-nav");
+// const skillsNavBlob = document.getElementById("skills-blob");
+// skillsNavContainer.addEventListener("click", (event) => {
+// 	console.log(event);
+// 	let rect = event.target.getBoundingClientRect();
+// 	let x = event.clientX - rect.left;
+// 	let y = event.clientY - rect.top;
+// 	skillsNavBlob.style.left = `${x}px`;
+// 	skillsNavBlob.style.top = `${y}px`;
+// });
+// 365
+// 122
--- script.js
+++ script.js
@@ -9,11 +9,27 @@ skillsBtns.forEach((button, i) => {
 		skillsBtns[i].classList.add("content-clicked");
 	});
 });
-function resetClasses() {
+const resetClasses = () => {
 	skillsContents.forEach((content) => {
 		content.classList.remove("show-content");
 	});
 	skillsBtns.forEach((button) => {
 		button.classList.remove("content-clicked");
 	});
-}
+};
+
+// MOVE BLOB TO MOUSE CLICK POSITION IN THE SKILLS NAV CONTAINER
+// const skillsNavContainer = document.getElementById("skills-nav");
+// const skillsNavBlob = document.getElementById("skills-blob");
+
+// skillsNavContainer.addEventListener("click", (event) => {
+// 	console.log(event);
+// 	let rect = event.target.getBoundingClientRect();
+// 	let x = event.clientX - rect.left;
+// 	let y = event.clientY - rect.top;
+// 	skillsNavBlob.style.left = `${x}px`;
+// 	skillsNavBlob.style.top = `${y}px`;
+// });
+
+// 365
+// 122
